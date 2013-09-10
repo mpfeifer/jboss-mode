5667fd716eef61d7f17e16b6dba49eacd21f5790
Matthias <mpfeifer77@gmail.com>
2013-09-10 20:09:25 +0200

    Initial Commit

---
+++ jboss-mode.el
@@ -0,0 +1,52 @@
+;; jboss-mode.el - minor mode for reading jboss logfiles
+;; Author: Matthias Pfeifer (mpfeifer77@gmail.com)
+;; -----------------------------------------------
+;;
+
+(defun mp::goto-next-WARN ()
+  "Go to next line with level of warn or lower."
+  (interactive)
+  (re-search-forward "^[0-9:,]+ \\(TRACE\\|DEBUG\\|INFO\\|WARN\\)")
+  )
+
+(defun mp::goto-next-INFO ()
+  "Go to next line with level of warn or lower."
+  (interactive)
+  (re-search-forward "^[0-9:,]+ \\(TRACE\\|DEBUG\\|INFO\\)")
+  )
+
+(defun mp::goto-next-ERROR ()
+  "Go to next line with level of error or lower."  
+  (interactive)
+  (re-search-forward "^[0-9:,]+ \\(TRACE\\|DEBUG\\|INFO\\|WARN\\|ERROR\\)")
+  )
+
+(defun mp::goto-next-DEBUG ()
+  "Go to next line with level of debug or lower."  
+  (interactive)
+  (re-search-forward "^[0-9:,]+ \\(TRACE\\|DEBUG\\)")
+  )
+
+(define-derived-mode jboss-mode read-only-mode "jboss"
+  "A major mode for viewing jboss logfiles."
+  :keymap jboss-mode-map
+
+    (local-set-key (kbd "C-w") 'mp::goto-next-WARN)
+    (local-set-key (kbd "C-e") 'mp::goto-next-ERROR)
+    (local-set-key (kbd "C-d") 'mp::goto-next-DEBUG)
+    (local-set-key (kbd "k") 'scroll-down-one-line)
+    (local-set-key (kbd "j") 'scroll-up-one-line)
+    (let ((jboss-font-lock-keywords
+           '(("^[0-9:,]+[[:space:]]" . font-lock-comment-face)
+             ("INFO" . font-lock-constant-face)
+             ("ERROR" . font-lock-preprocessor-face)
+             ("DEBUG" . font-lock-keyword-face)
+             ("WARN\\|WARNUNG" . font-lock-function-name-face)
+             ("FATAL\\|SCHWERWIEGEND" . font-lock-function-name-face)
+             ("\\[[a-zA-Z0-9.]+\\]" . font-lock-type-face)
+             ("([[:space:]A-Za-z0-9-]+)" . font-lock-builtin-face)
+             )))
+      (setq font-lock-defaults '(jboss-font-lock-keywords)))
+  )
+
+(provide 'jboss-mode)
